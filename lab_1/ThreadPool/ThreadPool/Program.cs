f9b6ff2474699090bf2f3e7f2043724a6a42e076
svoevremennaya <rusilkoizabella@mail.ru>
2021-10-04 18:53:33 +0300

    Initial commit

---
+++ lab_1/ThreadPool/ThreadPool/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPool
+{
+    public delegate void TaskDelegate();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the number of threads: ");
+            int numberThreads = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the number of tasks: ");
+            int numberTasks = Convert.ToInt32(Console.ReadLine());
+
+            TaskQueue taskQueue = new TaskQueue(numberThreads);
+
+            for (int i = 0; i < numberTasks; i++)
+            {
+                taskQueue.EnqueueTask(Print);
+            }
+
+            Console.ReadLine();
+        }
+
+        static public void Print()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.Name} executes task");
+        }
+    }
+}
---
+++ lab_1/ThreadPool/ThreadPool/TaskQueue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPool
+{
+    public class TaskQueue
+    {
+        public List<Thread> pool = new List<Thread>();
+        public Queue<TaskDelegate> taskQueue = new Queue<TaskDelegate>();
+
+        public TaskQueue(int threadNumber)
+        {
+            for (int i = 0; i < threadNumber; i++)
+            {
+                Thread thread = new Thread(ExecuteTask);
+                thread.Name = Convert.ToString(i + 1);
+                thread.IsBackground = true;
+                pool.Add(thread);
+                thread.Start();
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate taskDelegate)
+        {
+            taskQueue.Enqueue(taskDelegate);
+        }
+
+        public void ExecuteTask()
+        {
+            while (Thread.CurrentThread.IsBackground)
+            {
+                if (taskQueue.Count > 0)
+                {
+                    TaskDelegate task = taskQueue.Dequeue();
+                    if (task != null)
+                    {
+                        task();
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(50);
+                }
+            }
+        }
+    }
+}
