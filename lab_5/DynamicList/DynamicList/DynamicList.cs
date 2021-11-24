3e25ada322f11fa7cb0880ccc5ca50ca99853071
svoevremennaya <rusilkoizabella@mail.ru>
2021-11-24 19:38:30 +0300

    Added lab_5

---
+++ lab_5/DynamicList/DynamicList/DynamicList.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicList
+{
+    public class DynamicList<T> : IEnumerable
+    {
+        private T[] list;
+        public int Count { get; private set; }
+
+        public DynamicList()
+        {
+            list = new T[0];
+            Count = 0;
+        }
+
+        public DynamicList(int length)
+        {
+            list = new T[length];
+            Count = 0;
+        }
+        
+        public void Add(T item)
+        {
+            if (list.Length == 0)
+            {
+                Array.Resize(ref list, 1);
+            }
+            else if (list.Length == Count)
+            {
+                Array.Resize(ref list, list.Length * 2);
+            }
+
+            list[Count] = item;
+            Count++;
+        }
+
+        public void Remove(T item)
+        {
+            int index = Array.IndexOf(list, item);
+            if (index >= 0)
+            {
+                list = list.Where((val, ind) => ind != index).ToArray();
+                Count--;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= 0 && index < Count)
+            {
+                list = list.Where((val, ind) => ind != index).ToArray();
+                Count--;
+            }
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+            Array.Resize(ref list, 0);
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return list[index];
+            }
+
+            set
+            {
+                list[index] = value;
+            }
+        }
+
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return list[i];
+            }
+        }
+    }
+}
---
+++ lab_5/DynamicList/DynamicList/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicList<string> list = new DynamicList<string>();
+            list.Add("tv");
+            list.Add("smartphone");
+            PrintDynamicList<string>(list);
+            Console.WriteLine("Number of elements in list: " + list.Count);
+
+            list.Add("fridge");
+            list.Add("tablet");
+            list.Add("laptop");
+            PrintDynamicList<string>(list);
+            Console.WriteLine("Number of elements in list: " + list.Count);
+
+            list.Remove("tablet");
+            PrintDynamicList<string>(list);
+            Console.WriteLine("Number of elements in list: " + list.Count);
+
+            list.RemoveAt(3);
+            PrintDynamicList<string>(list);
+            Console.WriteLine("Number of elements in list: " + list.Count);
+
+            list.Clear();
+            PrintDynamicList<string>(list);
+            Console.WriteLine("Number of elements in list: " + list.Count);
+            Console.ReadLine();
+        }
+
+        static void PrintDynamicList<T>(DynamicList<T> list)
+        {
+            foreach(var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
